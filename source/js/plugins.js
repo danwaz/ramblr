52eb74c52b3cead564bd03be1a0cbbee0aa1ad6f
Dan Wasilewski <heydanwaz@gmail.com>
2012-08-19 23:27:36 -0400

    initial commit

---
+++ source/js/plugins.js
@@ -0,0 +1,67 @@
+window.log = function f(){ log.history = log.history || []; log.history.push(arguments); if(this.console) { var args = arguments, newarr; args.callee = args.callee.caller; newarr = [].slice.call(args); if (typeof console.log === 'object') log.apply.call(console.log, console, newarr); else console.log.apply(console, newarr);}};
+(function(a){function b(){}for(var c="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),d;!!(d=c.pop());){a[d]=a[d]||b;}})
+(function(){try{console.log();return window.console;}catch(a){return (window.console={});}}());
+
+
+//images loaded
+(function(c,n){var k="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";c.fn.imagesLoaded=function(l){function m(){var b=c(h),a=c(g);d&&(g.length?d.reject(e,b,a):d.resolve(e));c.isFunction(l)&&l.call(f,e,b,a)}function i(b,a){b.src===k||-1!==c.inArray(b,j)||(j.push(b),a?g.push(b):h.push(b),c.data(b,"imagesLoaded",{isBroken:a,src:b.src}),o&&d.notifyWith(c(b),[a,e,c(h),c(g)]),e.length===j.length&&(setTimeout(m),e.unbind(".imagesLoaded")))}var f=this,d=c.isFunction(c.Deferred)?c.Deferred():
+0,o=c.isFunction(d.notify),e=f.find("img").add(f.filter("img")),j=[],h=[],g=[];e.length?e.bind("load.imagesLoaded error.imagesLoaded",function(b){i(b.target,"error"===b.type)}).each(function(b,a){var e=a.src,d=c.data(a,"imagesLoaded");if(d&&d.src===e)i(a,d.isBroken);else if(a.complete&&a.naturalWidth!==n)i(a,0===a.naturalWidth||0===a.naturalHeight);else if(a.readyState||a.complete)a.src=k,a.src=e}):m();return d?d.promise(f):f}})(jQuery);
+
+//centered grid
+$.Isotope.prototype._getCenteredMasonryColumns = function() {
+	this.width = this.element.width();
+
+	var parentWidth = this.element.parent().width();
+
+	// i.e. options.masonry && options.masonry.columnWidth
+	var colW = this.options.masonry && this.options.masonry.columnWidth ||
+	// or use the size of the first item
+	this.$filteredAtoms.outerWidth(true) ||
+	// if there's no items, use size of container
+	parentWidth;
+
+	var cols = Math.floor( parentWidth / colW );
+	cols = Math.max( cols, 1 );
+
+	// i.e. this.masonry.cols = ....
+	this.masonry.cols = cols;
+	// i.e. this.masonry.columnWidth = ...
+	this.masonry.columnWidth = colW;
+};
+
+$.Isotope.prototype._masonryReset = function() {
+	// layout-specific props
+	this.masonry = {};
+	// FIXME shouldn't have to call this again
+	this._getCenteredMasonryColumns();
+	var i = this.masonry.cols;
+	this.masonry.colYs = [];
+	while (i--) {
+		this.masonry.colYs.push( 0 );
+	}
+};
+
+$.Isotope.prototype._masonryResizeChanged = function() {
+	var prevColCount = this.masonry.cols;
+	// get updated colCount
+	this._getCenteredMasonryColumns();
+	return ( this.masonry.cols !== prevColCount );
+};
+
+$.Isotope.prototype._masonryGetContainerSize = function() {
+var unusedCols = 0,
+i = this.masonry.cols;
+// count unused columns
+while ( --i ) {
+	if ( this.masonry.colYs[i] !== 0 ) {
+		break;
+	}
+	unusedCols++;
+}
+
+return {
+	height : Math.max.apply( Math, this.masonry.colYs ),
+	// fit container to columns that have been used;
+	width : (this.masonry.cols - unusedCols) * this.masonry.columnWidth
+	};
+};
---
+++ source/js/site.js
@@ -0,0 +1,145 @@
+(function(){
+	//autocomplete init
+	new google.maps.places.Autocomplete(document.getElementById('autocomplete'));
+
+	//vars
+	var map,service, infowindow;
+
+	//input handler
+	$('input[type="submit"]').click(function(e){
+		e.preventDefault();
+		var searchQuery = $('#autocomplete').val();
+		var $removeable = $('.main').find('.instagram');
+		$('.main').isotope('remove', $removeable);
+		init(searchQuery);
+	});
+
+	var init = function(searchQuery) {
+		var initCenter = new google.maps.LatLng(40,-74);
+
+		map = new google.maps.Map(document.getElementById('map'), {
+			mapTypeId: google.maps.MapTypeId.ROADMAP,
+			center : initCenter,
+			zoom: 15
+		});
+
+		var request = {
+			query: searchQuery
+		};
+
+		service = new google.maps.places.PlacesService(map);
+		service.textSearch(request, callback);
+	}
+
+	var createMarker =function(lat, lng){
+		var marker = new google.maps.Marker({
+			position: new google.maps.LatLng(lat, lng),
+			map: map
+		});
+	}
+
+	var callback = function(results, status) {
+		if (status == google.maps.places.PlacesServiceStatus.OK) {
+			var lat = results[0].geometry.location.Xa;
+			var lng = results[0].geometry.location.Ya;
+			var center = new google.maps.LatLng(lat, lng);
+			map.panTo(center);
+			createMarker(lat, lng);
+			console.log(lat + ':::::' + lng)
+			getLocation(lat, lng);
+		}
+	}
+
+	var getLocation = function(lat, lng){
+		$.ajax({
+			url : "https://api.foursquare.com/v2/venues/search?ll="+ lat + "," + lng +"&oauth_token=0GMVSGB5PKWRAIRHXUFBC5MSNCIV2W0ICC0QMKZFRJGNSYBH&v=20120801",
+			dataType : 'jsonp',
+			success : getIgLocation
+		});
+	}
+
+	var getIgLocation = function(data){
+		var foursquareId = data.response.venues[0].id;
+		console.log(foursquareId);
+		$.ajax({
+			url : "https://api.instagram.com/v1/locations/search?foursquare_v2_id="+ foursquareId +"&callback=?&amp;client_id=b93756e565794360942f1eba0831c90c",
+			dataType : 'jsonp',
+			success : processLocation
+		});
+	}
+
+	var processLocation = function(data){
+		var locationId = data.data[0].id;
+		getPhotos("https://api.instagram.com/v1/locations/"+ locationId +"/media/recent?callback=?&amp;client_id=b93756e565794360942f1eba0831c90c");
+	}
+
+	//instagram
+	var index = 0,
+		nextPage = "",
+		photoArr = [];
+
+	var getPhotos = function(url){
+		$.ajax({
+			url : url,
+			dataType : 'jsonp',
+			success : processPhotos
+		});
+	};
+
+	var processPhotos = function(data){
+		photoArr = [];
+		$.each(data.data, function(index, photo){
+			photoArr.push(photo);
+		});
+
+		displayPhotos(photoArr);
+		nextPage =  data.pagination.next_max_id;
+	};
+
+	var displayPhotos = function(photos){
+		$.each(photos, function(index, photo){
+			//console.log(photo);
+			var photoItem = $('<div class="item instagram"><a href="'+ photo.link +'" target="_blank"><img src="' + photo.images.low_resolution.url + '" width="310" height="310"/><div class="overlay"><h2>'+ photo.likes.count +' &hearts;</h2></div></a></div>');
+			$('.main').isotope('insert', photoItem);
+		});
+		$('.instagram').hover(function(){
+			$(this).find('.overlay').fadeIn('fast');
+		}, function(){
+			$(this).find('.overlay').fadeOut('fast');
+		});
+	};
+
+	var loadMore = function(url){
+		$.getJSON(url, processMore);
+	};
+
+	var processMore = function(data){
+		$.each(data.data, function(index, photo){
+			var photoItem = $('<div class="item instagram"><a href="'+ photo.link +'" target="_blank"><img src="' + photo.images.low_resolution.url + '" width="310" height="310"/><div class="overlay"><h2>'+ photo.likes.count +' &hearts;</h2></div></a></div>');
+			$('#container').isotope('insert', photoItem);
+		});
+		nextPage =  data.pagination.next_max_id;
+	};
+
+	var initGrid = function(){
+		//$('#loader').remove();
+		$('.main').isotope({
+			// options
+			itemSelector : '.item',
+			masonry: {
+				columnWidth: 160
+			},
+		});
+		// $('#filters a').click(function(){
+		// 	var selector = $(this).attr('data-filter');
+		// 	$('#container').isotope({ filter: selector });
+		// 	return false;
+		// });
+	};
+	initGrid();
+
+})();
+
+
+
+
